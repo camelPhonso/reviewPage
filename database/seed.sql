6f0a0865f0ff6d669fb00893f064e9098240bd44
Alphonso <114600712+camelPhonso@users.noreply.github.com>
2023-06-03 00:15:58 +0100

    database initialised

--- database/seed.sql
+++ database/seed.sql
@@ -1,11 +1,15 @@
-INSERT INTO users VALUES
-  (1, 'Ciccio', 'ahsktodtport', 'Colleague at herbivores'),
-  (2, 'Patricio', 'shtkepdnfotas', 'Met him once on the train'),
-  (3, 'R. L. Stine', 'sghepasktdot', 'Never met the man')
+BEGIN;
+
+INSERT INTO users (name, hash, relation) VALUES
+  ('John Doe', 'hash1', 'friend'),
+  ('Jane Smith', 'hash2', 'colleague'),
+  ('Alice Johnson', 'hash3', 'family')
 ON CONFLICT DO NOTHING;
 
-INSERT INTO reviews VALUES
-  (1, 2, "Absolutely the best commuter I've ever laid eyes on.", "2023-05-02 13:14:53"),
-  (2, 3, "I don't understand what this is about.", "2023-06-01 15:07:25"),
-  (3, 1, "He was alright.", "2023-06-01 17:03:51")
+INSERT INTO reviews (user_id, content) VALUES
+  (1, 'Great person to be around.'),
+  (2, 'Very knowledgeable and helpful.'),
+  (3, 'Always there when you need her.')
 ON CONFLICT DO NOTHING;
+
+COMMIT;
