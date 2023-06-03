609ff187fa4fd34a0c5dcba4765046c0b292a2ec
Alphonso <114600712+camelPhonso@users.noreply.github.com>
2023-06-03 21:36:27 +0100

    bug fix - middleware now correctly evaluates signedCookies

--- src/routes/home.js
+++ src/routes/home.js
@@ -1,14 +1,14 @@
 const { layout } = require("../templates/layout.js");
-const { home } = require("../templates/home.js");
+const { homePage } = require("../templates/home.js");
 const { form } = require("../templates/form.js");
 const { selectAllReviews } = require("../model/reviews.js");
 
 function get(req, res) {
-  const session = req.sessions;
+  const session = req?.sessions;
   const allReviews = selectAllReviews();
 
   const title = "Review Me";
-  const body = home(allReviews, form(session));
+  const body = homePage(allReviews, form(session));
 
   res.send(layout(title, body));
 }
