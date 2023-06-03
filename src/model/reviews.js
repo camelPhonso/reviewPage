313cd1994eccf1d1171721e2da18677fab4dcf90
Alphonso <114600712+camelPhonso@users.noreply.github.com>
2023-06-03 22:55:21 +0100

    added JOIN to display complete info in GET(/)

--- src/model/reviews.js
+++ src/model/reviews.js
@@ -1,7 +1,13 @@
 const db = require("../database/db.js");
 
 const select_reviews = db.prepare(/*sql*/ `
-  SELECT * FROM reviews
+  SELECT 
+    reviews.content AS content,
+    reviews.created_at AS created_at,
+    users.name AS username,
+    users.relation AS relation
+  FROM reviews
+  JOIN users ON reviews.user_id = users.id
 `);
 
 function selectAllReviews() {
